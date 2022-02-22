38599c529b0581cb996ad921fe60bc24a912af6d
Utku EROL <utkuegeerol@gmail.com>
2022-02-23 00:15:03 +0300

    format document ...

--- src/server.js
+++ src/server.js
@@ -1,62 +1,62 @@
 import { createServer, Model } from 'miragejs'
 
 createServer({
-    models: {
-        todo: Model.extend({}),
-      },
-      seeds(server) {
+  models: {
+    todo: Model.extend({}),
+  },
+  seeds(server) {
 
-        server.create('todo', {
-          name: 'Get backups MYSQL',
-          checked: false,
-        })
-        server.create('todo', {
-          name: 'Clean cache Prod. Server',
-          checked: false,
-        })
-        server.create('todo', {
-          name: 'Learn Tailwind',
-          checked: false,
-        })
-      },
-    routes(){
-        this.namespace = 'api'
+    server.create('todo', {
+      name: 'Get backups MYSQL',
+      checked: true,
+    })
+    server.create('todo', {
+      name: 'Clean cache Prod. Server',
+      checked: false,
+    })
+    server.create('todo', {
+      name: 'Learn Tailwind',
+      checked: true,
+    })
+  },
+  routes() {
+    this.namespace = 'api'
 
-        this.get('/')
-     
-        this.get('/todos')
-     
-        this.get('/todos/:id')
-     
-        this.post('/todos', (schema, request) => {
-          let attrs = JSON.parse(request.requestBody)
-    
-          return schema.todos.create(attrs)
-        })
-    
-        this.patch('/todos/:id', (schema, request) => {
-          let newAttrs = JSON.parse(request.requestBody)
-          let id = request.params.id
-          let todo = schema.todos.find(id)
-    
-          return todo.update(newAttrs)
-        })
-    
-        this.patch('/todos/checked/:id', (schema, request) => {
+    this.get('/')
 
-          console.log(request.params)
+    this.get('/todos')
 
-          let newAttrs = JSON.parse(request.requestBody)
+    this.get('/todos/:id')
 
-          console.log(newAttrs)
-          let id = request.params.id
-          let todo = schema.todos.find(id)
+    this.post('/todos', (schema, request) => {
+      let attrs = JSON.parse(request.requestBody)
 
-          console.log(todo)
-          
-          return todo.update(newAttrs)
-        })
-    
-        this.del('/todos/:id')
-    }
+      return schema.todos.create(attrs)
+    })
+
+    this.patch('/todos/:id', (schema, request) => {
+      let newAttrs = JSON.parse(request.requestBody)
+      let id = request.params.id
+      let todo = schema.todos.find(id)
+
+      return todo.update(newAttrs)
+    })
+
+    this.patch('/todos/checked/:id', (schema, request) => {
+
+      console.log(request.params)
+
+      let newAttrs = JSON.parse(request.requestBody)
+
+      console.log(newAttrs)
+      let id = request.params.id
+      let todo = schema.todos.find(id)
+
+      console.log(todo)
+
+      return todo.update(newAttrs)
+    })
+
+    this.del('/todos/:id')
+  }
 })
