4860132950331d911b4dc5bb459534a5fcc8ebe4
Utku EROL <utkuegeerol@gmail.com>
2022-02-22 11:03:12 +0300

    fetched todo list from get api in useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import React,{useEffect,useState} from "react";
+
+
 function App() {
+
+  const [todo,setTodo] = useState(null);
+  
+  useEffect(()=>{
+    fetch('/api/todo_list')
+    .then(res=>res.json())
+    .then(json=>setTodo(json.todo))
+    .catch(err=>console.log(err))
+  },[])
+
+
+
+
   return (
-    <div className="App">
-      Hello World
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col">
+
+        </div>
+      </div>
     </div>
+
   );
 }
 
